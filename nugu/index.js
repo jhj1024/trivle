e5e8249b53ad47bf4775486e56aae20f561a163b
jhj1024 <54670748+jhj1024@users.noreply.github.com>
2019-11-10 19:00:21 +0900

    Update index.js
    set set

--- nugu/index.js
+++ nugu/index.js
@@ -66,14 +66,14 @@ function Listen_Tip(){
 
 function Set_List(DestinationForSet) { //몇박몇일에 대한 데이터도 인자로 추가
   const hey = DestinationForSet.type; //국내/해외인지 엔티티 타입(in/ hey)
-  const DestinationForset = DestinationForSet.value; //여행지 이름 (런던, 파리, 강원도)
+  const Destinationvalue = DestinationForSet.value; //여행지 이름 (런던, 파리, 강원도)
   
 
-  console.log('DestinationForSet: ' + typeof(DestinationForSet));
+  console.log('Destinationvalue: ' + typeof(Destinationvalue));
   var sql = 'Create table ? SELECT * FROM IS;'
 
     pool.getConnection(function(err, connection) {
-        connection.query(sql, DestinationForset.toString(), function(err, rows) {
+        connection.query(sql, Destinationvalue, function(err, rows) {
           if (err) {
             console.log('Error Create Query.', err);
           } 
@@ -82,7 +82,7 @@ function Set_List(DestinationForSet) { //몇박몇일에 대한 데이터도 인
           }
         });
     })
-  return {DestinationForset}
+  return {Destinationvalue}
 }
 
 function Delete_List(DestinationForDelete) {
