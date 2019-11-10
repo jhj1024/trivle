dc1a2244d9b79c7687b7661b212c6df6616329fa
leesoyoung <sy9612@naver.com>
2019-11-10 17:17:54 +0900

    Update index.js

--- nugu/index.js
+++ nugu/index.js
@@ -203,7 +203,7 @@ class NPKRequest {
             
       case 'Listen_Tip':
       result = Listen_Tip() //함수 실행
-      console.log(result.Tip)
+      console.log(result)
       console.log('@@@@@@@')
       npkResponse.Listen_Tip_Output(result) //함수 결과를 output 파라미터에 저장
       break;
