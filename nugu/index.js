264509ad0e5b02d85b7350a02a899ff9e11591e5
leesoyoung <sy9612@naver.com>
2019-11-13 21:05:53 +0900

    Update index.js

--- nugu/index.js
+++ nugu/index.js
@@ -188,10 +188,8 @@ function Delete_List(DestinationForDelete) {
 //--------------------------------------------------
 
 //SY------------------------------------------------
-Listen_Tip(function(result){
-           NPKRequest.actionRequest(result);    
-});
 
+var tasks = [
 function Listen_Tip(callback){
     //let Destination = DestinationForTip;
     
@@ -207,61 +205,15 @@ function Listen_Tip(callback){
                 TIP = rows[rand].T;
 
                 console.log('1' + TIP);
-                //return {TIP};
+                return {TIP};
             })
         }
     });
 
     console.log('2'+TIP);
-    callback(TIP)
+
     return {TIP};
-}
-
-//지현---------------------------------------------------
-function Listen_List(DestinationForListen) { //읽을 카테고리 데이터도 인자로 추가
-  let Destination = DestinationForListen;
-  var listen = '다쿠아즈';
-
-  console.log('Destination: ' + Destination); 
-  console.log('Destination type: ' + typeof(Destination)); 
-
-  pool.getConnection(function(err, connection) {
-    if(err){
-      console.log('DB_connection_err :' + err);
-    }
-    else{
-      var sqlForListen = "SELECT * FROM " + Destination + ";"
-      connection.query(sqlForListen, function(err, rows) {
-        if (err) {
-          console.log('query_err :' + err);          
-        } 
-        else {
-          console.log(rows[0])
-          listen = rows[0].P     
-        }
-      })
-
-    }
-
-  })
-  return {listen} 
-}
-
-//--------------------------------------------------------------
-class NPKRequest {
-  constructor (httpReq) { //httpReq의 body에서 context와 action 추출
-    console.log(httpReq.body)
-    this.context = httpReq.body.context
-    this.action = httpReq.body.action
-    //console.log(`NPKRequest: ${JSON.stringify(this.context)}, ${JSON.stringify(this.action)}`)
-  }
-  
-  //reqeust 처리
-  do(npkResponse) { 
-    this.actionRequest(npkResponse)
-  }
-
-  actionRequest(npkResponse) {
+}, NPKRequest.actionRequest(npkResponse) {
     console.log('actionRequest')
     console.dir(this.action)
 
@@ -309,6 +261,106 @@ class NPKRequest {
             
     }
   }
+    ];
+async.waterfall(tasks, function (err) {
+    if (err)
+        console.log('err');
+    else
+        console.log('done');
+    connection.end();
+});
+//지현---------------------------------------------------
+function Listen_List(DestinationForListen) { //읽을 카테고리 데이터도 인자로 추가
+  let Destination = DestinationForListen;
+  var listen = '다쿠아즈';
+
+  console.log('Destination: ' + Destination); 
+  console.log('Destination type: ' + typeof(Destination)); 
+
+  pool.getConnection(function(err, connection) {
+    if(err){
+      console.log('DB_connection_err :' + err);
+    }
+    else{
+      var sqlForListen = "SELECT * FROM " + Destination + ";"
+      connection.query(sqlForListen, function(err, rows) {
+        if (err) {
+          console.log('query_err :' + err);          
+        } 
+        else {
+          console.log(rows[0])
+          listen = rows[0].P     
+        }
+      })
+
+    }
+
+  })
+  return {listen} 
+}
+
+//--------------------------------------------------------------
+class NPKRequest {
+  constructor (httpReq) { //httpReq의 body에서 context와 action 추출
+    console.log(httpReq.body)
+    this.context = httpReq.body.context
+    this.action = httpReq.body.action
+    //console.log(`NPKRequest: ${JSON.stringify(this.context)}, ${JSON.stringify(this.action)}`)
+  }
+  
+  //reqeust 처리
+  do(npkResponse) { 
+    this.actionRequest(npkResponse)
+  }
+
+  /*actionRequest(npkResponse) {
+    console.log('actionRequest')
+    console.dir(this.action)
+
+    //액션 이름과 파라미터 저장(모두 nugu play kit의 액션과 파라미터 이름임)
+    const actionName = this.action.actionName 
+    const parameters = this.action.parameters
+    let result = null
+
+    console.log('액션먼저?')
+    switch (actionName) {
+      case 'Set_List':
+      let DestinationForSet = parameters.DestinationForSet //여행지
+      //몇박몇일에 대한 데이터도 파라미터로 추가 
+
+      result = Set_List(DestinationForSet.value) //함수 실행
+      console.log(result)
+      npkResponse.Set_List_Output(result) //함수 결과를 output 파라미터에 저장
+      break;
+
+      case 'Delete_List':
+      const DestinationForDelete = parameters.DestinationForDelete //여행지
+      
+      result = Delete_List(DestinationForDelete) //함수 실행
+      console.log(result)
+      npkResponse.Delete_List_Output(result) //함수 결과를 output 파라미터에 저장
+      break;
+
+      case 'Listen_List':
+      const DestinationForListen = parameters.DestinationForListen //여행지
+      //읽을 카테고리 데이터도 파라미터로 추가
+
+      const result1 = Listen_List(DestinationForListen.value) //함수 실행
+      console.log('함수결과' + result1.listen)
+      npkResponse.Listen_List_Output(result1) //함수 결과를 output 파라미터에 저장
+      break;
+            
+      case 'Listen_Tip':
+      //const DestinationForTip = parameters.DestinationForTip //여행지
+      console.log('어떤거 먼저?')
+      result = Listen_Tip() //함수 실행
+      console.log('3'+result)
+      console.log('@@@@@@@')
+      npkResponse.Listen_Tip_Output(result) //함수 결과를 output 파라미터에 저장
+      break;
+            
+    }
+  }*/
 }
 
 class NPKResponse {
