963424e51d16a9e778416bddcf7398599dd9cce2
jhj1024 <54670748+jhj1024@users.noreply.github.com>
2019-11-14 02:45:56 +0900

    edit index
    edit index

--- trivle_flask/index.py
+++ trivle_flask/index.py
@@ -6,16 +6,40 @@ Created on Thu Nov 14 00:27:54 2019
 """
 
 import pymysql
+'''
 conn = pymysql.connect(host='223.194.46.68', user='guest', password='1q2w3e4r!', db='my_db', charset='utf8mb4')
 
-'''
+
 with conn.cursor() as cursor:
     sql = 'SELECT * FROM my_db.adtext'
     cursor.execute(sql)
     temp1 = cursor.fetchall()
 '''
-def listen_list(parameters):
+
+def Set_List(parameters):
     print('parameters')
     print(parameters)
+    
+    hello = {'destination':'hello'}
+    return hello
+
+def Delete_List(parameters):
+    print('parameters')
+    print(parameters)
+    
+    hello = {'destination':'hello'}
+    return hello
+
+def Listen_List(parameters):
+    print('parameters')
+    print(parameters)
+    
+    hello = {'destination':'hello'}
+    return hello
+
+def Listen_Tip(parameters):
+    print('parameters')
+    print(parameters)
+    
     hello = {'destination':'hello'}
     return hello
