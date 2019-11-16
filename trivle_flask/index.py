1707742b1b0da6c21ad4e83cf2404efe82ccff4e
JiHongKim15 <46816743+JiHongKim15@users.noreply.github.com>
2019-11-17 04:10:51 +0900

    Update index.py

--- trivle_flask/index.py
+++ trivle_flask/index.py
@@ -34,7 +34,7 @@ def Set_List(parameters):
 
     cur = conn.cursor()
     print(333333333333333333333333333333)
-    print(int(parameters['FewDay']['value']))
+    print(type(parameters['FewDay']['value']))
     if(parameters['DestinationForSet']['type'] == 'HEY'):
         if(int(parameters['FewDay']['value'])<=7):
             print(11111111111111111111111)
